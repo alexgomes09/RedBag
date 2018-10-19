c4f6feae3409a030a46c56261a9a72b3ebd8ad99
Alex Gomes <alex_09hg@yahoo.com>
2018-10-19 11:55:14 -0400

    update

--- models/Donor.js
+++ models/Donor.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 8;;
+    SALT_WORK_FACTOR = 8;
 
 var donorSchema = new Schema({
     emailAddress: {
